dbb925cd2524603c9ad8b4678ca339545907e4ac
Alex Yopp <alexyopp@gmail.com>
2024-06-05 17:43:13 -0700

    Fixed incorrect MissionHistory type references.

--- XvTPilotEditor/Models/MissionHistory.cs
+++ XvTPilotEditor/Models/MissionHistory.cs
@@ -4,11 +4,11 @@ namespace XvTPilotEditor.Models
 {
     class MissionHistory
     {
-        public List<TrainingMissionResult>? Training    { get; set; }
-        public List<MeleeMissionResult>?    Melee       { get; set; }
-        public List<TournamentResult>?      Tournament  { get; set; }
-        public List<CombatMissionResult>?   Combat      { get; set; }
-        public List<BattleResult>?          Battle      { get; set; }
-        public List<CampaignResult>?        Campaign    { get; set; }
+        public List<TrainingResult>?    Training    { get; set; }
+        public List<MeleeResult>?       Melee       { get; set; }
+        public List<TournamentResult>?  Tournament  { get; set; }
+        public List<CombatResult>?      Combat      { get; set; }
+        public List<BattleResult>?      Battle      { get; set; }
+        public List<CampaignResult>?    Campaign    { get; set; }
     }
 }
