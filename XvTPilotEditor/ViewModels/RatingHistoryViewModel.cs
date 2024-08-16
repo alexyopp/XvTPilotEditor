180e2bd91fe20ffbf3f5de8d4c041d677ca45441
Alex Yopp <alexyopp@gmail.com>
2024-08-16 10:26:41 -0700

    Filled in view and viewmodel for Rating History.

--- XvTPilotEditor/ViewModels/RatingHistoryViewModel.cs
+++ XvTPilotEditor/ViewModels/RatingHistoryViewModel.cs
@@ -1,12 +1,39 @@
-﻿using XvTPilotEditor.Models;
+﻿using System.Collections.Generic;
+using System;
+using XvTPilotEditor.Models;
 
 namespace XvTPilotEditor.ViewModels
 {
+    public class RatingHistoryItem
+    {
+        public uint MissionAchieved { get; set; }
+
+        public RatingHistoryItem(uint missionAchieved)
+        {
+            this.MissionAchieved = missionAchieved;
+        }
+    }
+
     public class RatingHistoryViewModel : PageViewModel
     {
+        public Dictionary<PilotRating, RatingHistoryItem> PlayerRankHistory { get; }
+
+        private PilotModel activePilotModel;
+
         internal RatingHistoryViewModel(PilotModel pilotModel)
             : base(pilotModel)
         {
+            this.activePilotModel = pilotModel;
+
+            this.PlayerRankHistory = new Dictionary<PilotRating, RatingHistoryItem>();
+
+            foreach (PilotRating rating in Enum.GetValues<PilotRating>())
+            {
+                uint missionNumber = activePilotModel.RatingHistory.RatingAchievedOnMissionNumber[rating];
+                PlayerRankHistory.Add(
+                    rating,
+                    new RatingHistoryItem(missionNumber));
+            }
         }
     }
 }
