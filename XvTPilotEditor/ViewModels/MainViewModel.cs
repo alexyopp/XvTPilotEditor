66555c2e8682b77859fbfd5452a53d5a20d4b9f7
Alex Yopp <alexyopp@gmail.com>
2026-04-09 15:40:16 -0700

    Moved plt/pl2 read/write functionality out of MainViewModel and into PilotViewModel

--- XvTPilotEditor/ViewModels/MainViewModel.cs
+++ XvTPilotEditor/ViewModels/MainViewModel.cs
@@ -18,130 +18,48 @@ namespace XvTPilotEditor.ViewModels
         public ICommand ExitCommand { get; private set; }
         public ICommand AboutCommand { get; private set; }
 
-        private CompletePilotRecord pilotRecord = new CompletePilotRecord();
+        private PilotViewModel? ActivePilotVM;
 
         public MainViewModel()
         {
             //AutoLoadFileData();     // TODO: Remove this line for production release; only for testing.
 
-            //ChangeActivePageCommand = new DelegateCommand(o => this.UpdateActivePageViewModel());
-            OpenCommand = new DelegateCommand(o => LoadFileData());
-            SaveCommand = new DelegateCommand(o => WriteFileData());
+            OpenCommand = new DelegateCommand(o => CreateNewPilot());       // From the user's perspective, we're opening pilot files, but from our perpsective, we're creating a Pilot Entity that contains the data of a plt and/or pl2 file
+            SaveCommand = new DelegateCommand(o => SavePilot());
             ExitCommand = new DelegateCommand(o => ExitApplication());
             AboutCommand = new DelegateCommand(o => ShowAboutMessage());
         }
 
-        private void AutoLoadFileData()
+        private void UpdateActivePilot(PilotViewModel newPilotVM)
         {
-            PLTFileRecord dataPlt = new PLTFileRecord();
-            string PltFileName = "C:\\Dev\\XvTPilotEditor\\XvTPilotEditor\\Assets\\LandoRasputi0.plt";
-            ReadFileBytes(PltFileName, ref dataPlt);
-
-            PL2FileRecord dataPl2 = new PL2FileRecord();
-            string Pl2FileName = "C:\\Dev\\XvTPilotEditor\\XvTPilotEditor\\Assets\\LandoRasputi0.pl2";
-            ReadFileBytes(Pl2FileName, ref dataPl2);
-
-            pilotRecord.Plt.FillFromPlt(dataPlt);
-            pilotRecord.Pl2.FillFromPl2(dataPl2);
+            // TODO: Validate the new pilot VM before setting it as active. If invalid, show an error message and do not update the active pilot.
+            ActivePilotVM = newPilotVM;
         }
 
-        private void LoadFileData()
+        // TODO: Remove after testing is complete
+        private void AutoLoadFileData()
+        {
+            PilotViewModel pilotVM = new PilotViewModel(
+                                            "AutoLoadPilot",
+                                            "C:\\Dev\\XvTPilotEditor\\XvTPilotEditor\\Assets\\LandoRasputi0.plt",
+                                            "C:\\Dev\\XvTPilotEditor\\XvTPilotEditor\\Assets\\LandoRasputi0.pl2");
+        }
+
+        private void CreateNewPilot()
         {
             NewPilotDialogue dialogue = new NewPilotDialogue();
             bool? result = dialogue.ShowDialog();
 
             if (result == true)
             {
-                PLTFileRecord dataPlt = new PLTFileRecord();
-                PL2FileRecord dataPl2 = new PL2FileRecord();
-
-                if (!string.IsNullOrWhiteSpace(dialogue.PltFilePath))
-                {
-                    ReadFileBytes(dialogue.PltFilePath, ref dataPlt);
-                    pilotRecord.Plt.FillFromPlt(dataPlt);
-                }
-
-                if (!string.IsNullOrWhiteSpace(dialogue.Pl2FilePath))
-                {
-                    ReadFileBytes(dialogue.Pl2FilePath, ref dataPl2);
-                    pilotRecord.Pl2.FillFromPl2(dataPl2);
-                }
+                PilotViewModel pilotVM = new PilotViewModel(dialogue.PilotName, dialogue.PltFilePath, dialogue.Pl2FilePath);
+                UpdateActivePilot(pilotVM);
             }
         }
 
-        static private void ReadFileBytes<T>(string FileName, ref T? data)
+        private void SavePilot()
         {
-            try
-            {
-                byte[] filebytes = File.ReadAllBytes(FileName);
-
-                GCHandle handle = GCHandle.Alloc(filebytes, GCHandleType.Pinned);
-                try
-                {
-                    data = Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject());
-                }
-                finally
-                {
-                    handle.Free();
-                }
-
-                if (filebytes.Length < Marshal.SizeOf<T>())
-                {
-                    Console.WriteLine("File is too small for the expected structure.");
-                    return;
-                }
-            }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("Error: File not found.");
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine($"I/O Error: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Unexpected error: {ex.Message}");
-            }
-        }
-
-        private void WriteFileData()
-        {
-            WriteFileBytes("Test.plt", pilotRecord.Plt.ToPltFileRecord());
-            WriteFileBytes("Test.pl2", pilotRecord.Pl2.ToPl2FileRecord());
-        }
-
-        private void WriteFileBytes<T>(string FileName, T data)
-        {
-            if (data == null)
-            {
-                Console.WriteLine("Error: Data to write is null.");
-                return;
-            }
-
-            try
-            {
-                int size = Marshal.SizeOf<T>();
-                byte[] filebytes = new byte[size];
-                GCHandle handle = GCHandle.Alloc(filebytes, GCHandleType.Pinned);
-                try
-                {
-                    Marshal.StructureToPtr(data, handle.AddrOfPinnedObject(), false);
-                }
-                finally
-                {
-                    handle.Free();
-                }
-                File.WriteAllBytes(FileName, filebytes);
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine($"I/O Error: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Unexpected error: {ex.Message}");
-            }
+            ActivePilotVM?.WriteFileData();
         }
 
         private void ExitApplication()
---
+++ XvTPilotEditor/ViewModels/PilotViewModel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using XvTPilotEditor.Models;
+using static XvTPilotEditor.Models.PilotFileSchema;
+
+namespace XvTPilotEditor.ViewModels
+{
+    class PilotViewModel
+    {
+        public string Name { get; private set; }        = string.Empty;
+        public string PltFileName { get; private set; } = string.Empty;
+        public string Pl2FileName { get; private set; } = string.Empty;
+
+        private PltRecord PltRecord = new PltRecord();
+        private Pl2Record Pl2Record = new Pl2Record();
+
+        public PilotViewModel(string name, string pltFileName, string pl2FileName)
+        {
+            UpdateName(name);
+            UpdatePltFileName(pltFileName);
+            UpdatePl2FileName(pl2FileName);
+        }
+
+        public void UpdateName(string newName)
+        {
+            // TODO: Add validation for the new name if necessary (e.g., check for null or empty string, conflict another PilotVM, etc.).
+            this.Name = newName;
+        }
+
+        public void UpdatePltFileName(string newPltFileName)
+        {
+            // TODO: Add validation for the new filename
+            this.PltFileName = newPltFileName;
+
+            PLTFileRecord dataPlt = new PLTFileRecord();
+            ReadFileBytes(newPltFileName, ref dataPlt);
+            this.PltRecord.FillFromPlt(dataPlt);
+        }
+
+        public void UpdatePl2FileName(string newPl2FileName)
+        {
+            // TODO: Add validation for the new filename
+            this.Pl2FileName = newPl2FileName;
+
+            PL2FileRecord dataPl2 = new PL2FileRecord();
+            ReadFileBytes(newPl2FileName, ref dataPl2);
+            this.Pl2Record.FillFromPl2(dataPl2);
+        }
+
+        // TODO: Repalce with writing to the actual files
+        public void WriteFileData()
+        {
+            WriteFileBytes("Test.plt", PltRecord.ToPltFileRecord());
+            WriteFileBytes("Test.pl2", Pl2Record.ToPl2FileRecord());
+        }
+
+        static private void ReadFileBytes<T>(string FileName, ref T? data)
+        {
+            try
+            {
+                byte[] filebytes = File.ReadAllBytes(FileName);
+
+                GCHandle handle = GCHandle.Alloc(filebytes, GCHandleType.Pinned);
+                try
+                {
+                    data = Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject());
+                }
+                finally
+                {
+                    handle.Free();
+                }
+
+                if (filebytes.Length < Marshal.SizeOf<T>())
+                {
+                    Console.WriteLine("File is too small for the expected structure.");
+                    return;
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Error: File not found.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"I/O Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+            }
+        }
+
+        static private void WriteFileBytes<T>(string FileName, T data)
+        {
+            if (data == null)
+            {
+                Console.WriteLine("Error: Data to write is null.");
+                return;
+            }
+
+            try
+            {
+                int size = Marshal.SizeOf<T>();
+                byte[] filebytes = new byte[size];
+                GCHandle handle = GCHandle.Alloc(filebytes, GCHandleType.Pinned);
+                try
+                {
+                    Marshal.StructureToPtr(data, handle.AddrOfPinnedObject(), false);
+                }
+                finally
+                {
+                    handle.Free();
+                }
+                File.WriteAllBytes(FileName, filebytes);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"I/O Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+            }
+        }
+    }
+}
